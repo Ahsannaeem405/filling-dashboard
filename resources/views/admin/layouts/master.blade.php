742a424bef55a8a299eceaa42258b875e9671e4f
Ali Haider <73309177+alihdr000@users.noreply.github.com>
2023-11-15 11:12:43 +0500

    fonts applied

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -42,7 +42,7 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@2.0.0/css/boxicons.min.css">
-
+    <link rel="stylesheet" href="{{ asset('app-assets/css/fonts.css') }}" type="text/css">
     <style>
         .tippy-popper{
             display: none;
